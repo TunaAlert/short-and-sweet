9554c419f081f4619948325809d09dc2948ec9c7
Unknown <The1crazy1cake@gmail.com>
2017-08-01 00:00:20 +0200

    player & camera movement

--- cat wizards/Assets/Scripts/PlayerController.cs
+++ cat wizards/Assets/Scripts/PlayerController.cs
@@ -2,16 +2,36 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(Rigidbody))]
 public class PlayerController : MonoBehaviour {
+
+	public float speed = 1;
 
 	[HideInInspector]
 	public Vector3 target;
 
+	Rigidbody rb;
+
 	void Start () {
-		
+		rb = GetComponent<Rigidbody> ();
 	}
 
-	void Update () {
-		
+	void FixedUpdate () {
+		float horz = Input.GetAxis ("Horizontal");
+		float vert = Input.GetAxis ("Vertical");
+
+		Vector2 run = new Vector2 (horz, vert);
+		if (run.sqrMagnitude > 1)
+			run.Normalize ();
+
+		run = run * speed;
+
+		Vector2 vel = new Vector2 (rb.velocity.x, rb.velocity.z);
+
+		if (Mathf.Abs (vel.sqrMagnitude) < Mathf.Abs (run.sqrMagnitude)) {
+			vel = Vector2.Lerp (vel, run, 0.75f);
+		}
+
+		rb.velocity = new Vector3 (vel.x, rb.velocity.y, vel.y);
 	}
 }
