f506af2e4229c92356c66d25127594c66e5bef46
Unknown <The1crazy1cake@gmail.com>
2017-07-31 23:21:41 +0200

    created unity project

---
+++ cat wizards/Assets/Scripts/CameraHost.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraHost : MonoBehaviour {
+
+	public GameObject defaultTarget;
+
+	private GameObject target;
+
+	void Start () {
+		SetDefaultTarget ();
+	}
+
+	void FixedUpdate () {
+		transform.position = Vector3.Lerp (transform.position, target.transform.position, 0.2f);
+	}
+
+	public void SetTarget(GameObject t){
+		target = t;
+	}
+
+	public void SetDefaultTarget(){
+		target = defaultTarget;
+	}
+}
---
+++ cat wizards/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+	[HideInInspector]
+	public Vector3 target;
+
+	void Start () {
+		
+	}
+
+	void Update () {
+		
+	}
+}
